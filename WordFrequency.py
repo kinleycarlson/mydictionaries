cca43c8ad42a6e8f6b479ad5adf87286fe327267
Kinley <Kinley@test.com>
2022-09-15 13:09:34 -0500

    hw

---
+++ WordFrequency.py
@@ -0,0 +1,17 @@
+infile = open('sometext.txt','r')
+
+dict = dict()
+
+text = infile.read()
+words = text.split(' ')
+
+print(words)
+
+for word in words:
+    if word in dict():
+        dict[word] += 1
+    else:
+        dict[word] = 1
+
+for k,v in dict.items():
+    print(k+': ',v)
